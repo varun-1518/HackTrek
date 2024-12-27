063955323a022818dea90c5cde0c391d269a5d1c
varun-1518 <varunpatel1504@gmail.com>
2024-12-27 17:19:47 +0530

    Cleaned up repository by removing unnecessary files

--- node_modules/@botpress/client/openapi.ts
+++
@@ -1,5 +0,0 @@
-import { api as publicOpenapi } from '@botpress/api'
-
-void publicOpenapi.exportClient('./src/gen', {
-  generator: 'opapi',
-})
--- node_modules/@botpress/client/tests/e2e/consts.ts
+++
@@ -1,2 +0,0 @@
-export const successMessage = '__SUCCESS__'
-export const failureMessage = '__FAIL__'
